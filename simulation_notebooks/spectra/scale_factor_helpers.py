fixed another unit conversion error

simulation_notebooks/spectra/scale_factor_helpers.py
@@ -1,15 +1,13 @@
-import matplotlib.pyplot as plt
 import numpy as np
 
-
 def a_matter_lambda(t, Omega_m, Omega_Lambda, h):
-    H_0 = 100 * h * 1/(8.086*1e19)  # in 1/s
+    H_0 = 100 * h * 1/(3.086*1e19)  # in 1/s
     a = np.power(Omega_m/Omega_Lambda * np.sinh(3/2*H_0*np.sqrt(Omega_Lambda)*t)**2 , 1/3)
     return a
 
 
 def t_matter_lambda(a, Omega_m, Omega_Lambda, h):
-    H_0 = 100 * h * 1/(8.086*1e19)  # in 1/s
+    H_0 = 100 * h * 1/(3.086*1e19)  # in 1/s
     t = 2/(3*H_0*np.sqrt(Omega_Lambda)) * np.arcsinh(np.sqrt(Omega_Lambda/Omega_m * a**3))
     return t
 
@@ -21,7 +19,7 @@ def delta_z(d_c, z_start, Omega_m, Omega_Lambda, h):
     """
     a_start = 1/(z_start + 1)
     c = 299792.458  # in km/s
-    t_tr = (d_c/h)*a_start/c *10**6 *8.086*10**13  # in s
+    t_tr = (d_c/h)*a_start/c *10**6 *3.086*10**13  # in s
     t_start = t_matter_lambda(a_start, Omega_m, Omega_Lambda, h)
 
     d_z = 1/a_start - 1/a_matter_lambda(t_start+t_tr, Omega_m, Omega_Lambda, h)
@@ -35,7 +33,7 @@ def corrected_z(d_c, z_start, Omega_m, Omega_Lambda, h):
     """
     a_start = 1/(z_start + 1)
     c = 299792.458  # in km/s
-    t_tr = (d_c/h)*a_start/c *10**6 *8.086*10**13  # in s
+    t_tr = (d_c/h)*a_start/c *10**6 *3.086*10**13  # in s
     t_start = t_matter_lambda(a_start, Omega_m, Omega_Lambda, h)
 
     corr_z = 1/a_matter_lambda(t_start-t_tr, Omega_m, Omega_Lambda, h) - 1