39df627ce0f806b9b07a95aae08bbf9ab0549b46
Jeremias Boucsein <jeremiasboucsein@gmail.com>
2025-11-24 11:02:41 +0100

    add correction for physical box expansion

--- simulation_notebooks/spectra/scale_factor_helpers.py
+++ simulation_notebooks/spectra/scale_factor_helpers.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.optimize import brentq
+
 
 def a_matter_lambda(t, Omega_m, Omega_Lambda, h):
     H_0 = 100 * h * 1/(3.086*1e19)  # in 1/s
@@ -22,7 +24,7 @@ def delta_z(d_c, z_start, Omega_m, Omega_Lambda, h):
     t_tr = (d_c/h)*a_start/c *10**6 *3.086*10**13  # in s
     t_start = t_matter_lambda(a_start, Omega_m, Omega_Lambda, h)
 
-    d_z = 1/a_start - 1/a_matter_lambda(t_start+t_tr, Omega_m, Omega_Lambda, h)
+    d_z = 1/a_matter_lambda(t_start-t_tr, Omega_m, Omega_Lambda, h) - 1/a_start 
     return d_z
 
 
@@ -40,6 +42,32 @@ def corrected_z(d_c, z_start, Omega_m, Omega_Lambda, h):
     return corr_z
 
 
+def t_box_rootfind(t_box, d_c, z_start, Omega_m, Omega_Lambda, h):
+    c = 299792.458  # in km/s
+    d_c = d_c * 3.086*1e19  # in km
+    t_2 = t_matter_lambda(1/(z_start+1), Omega_m, Omega_Lambda, h)
+    return d_c/c * a_matter_lambda(t_2 - t_box, Omega_m, Omega_Lambda, h) - t_box
+
+
+def solve_for_correct_t_box(d_c, z_start, Omega_m, Omega_Lambda, h):
+    c = 299792.458  # in km/s
+    d_c_correct = d_c/h
+    t_start = d_c_correct/(c*(z_start+1)) *10**6 *3.086*10**13  # in s
+
+    t_box = brentq(t_box_rootfind, t_start*0.98, t_start, args=(d_c_correct, z_start, Omega_m, Omega_Lambda, h))
+
+    return t_box
+
+
+def corrected_z_exact(d_c, z_start, Omega_m, Omega_Lambda, h):
+
+    t_2 = t_matter_lambda(1/(z_start+1), Omega_m, Omega_Lambda, h)
+
+    t_box = solve_for_correct_t_box(d_c, z_start, Omega_m, Omega_Lambda, h)
+
+    return 1/a_matter_lambda(t_2 - t_box, Omega_m, Omega_Lambda, h) - 1 
+
+
 if __name__ == "__main__":
     #t_vals = np.logspace(0, 18, 1000)
     z_start = 2
@@ -49,15 +77,18 @@ if __name__ == "__main__":
     h = 0.7
     lambda_HI = 1215.67
 
-    dz = delta_z(d_c, z_start, Omega_m, Omega_Lambda, h)
+    exact_z = corrected_z_exact(d_c, z_start, Omega_m, Omega_Lambda, h)
+    print(exact_z)
 
-    dlambda = lambda_HI * dz
+    #dz = corrected_z(d_c, z_start, Omega_m, Omega_Lambda, h)
 
-    print(dlambda, dz)
+    #dlambda = lambda_HI * dz
 
-    estimated_error = 100*(d_c*1/(z_start-dz+1) - d_c*1/(z_start+1))/d_c  # in percent
+    #print(dlambda, dz)
 
-    print(f"Estimated error: {estimated_error:.2f}%")
+    #estimated_error = 100*(d_c*1/(z_start-dz+1) - d_c*1/(z_start+1))/d_c  # in percent
+
+    #print(f"Estimated error: {estimated_error:.2f}%")
 
 
 
